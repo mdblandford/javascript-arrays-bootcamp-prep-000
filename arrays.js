8f278d0fe44c501bea1be46880ed894f764120fc
Michael Blandford <mdblandford@gmail.com>
2018-03-26 18:48:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,4 +7,3 @@ function accessElementInArray(array,index){return array[index]}
 function destructivelyRemoveElementFromBeginningOfArray(array){array.shift();return array}
 function removeElementFromBeginningOfArray(array){return array.slice(1)}
 function destructivelyRemoveElementFromEndOfArray(array){array.pop();return array}
-function destructivelyRemoveElementFromEndOfArray(array){return array.slice(3)}
